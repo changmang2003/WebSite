bf10d3f2fdaf88fa47332c075e5c182e152a1d12
changmang2003 <changmang.yu@gmail.com>
2022-02-10 16:20:53 +0800

    Update get-starter.js

--- get-starter.js
+++ get-starter.js
@@ -143,7 +143,7 @@ function showPosition(position) {
   };
   
   $.get("https://script.google.com/macros/s/AKfycbw1X6eY1UFUTQnuxXmqEj82BiiymItZae66x89OoKz-UNE4e-9FH4AyFx9iHgXL3pz6/exec", parameter);
-    document.getElementById("map-link").innerHTML = document.getElementById("demo").textContent+"完成："+ latlon;    
+    document.getElementById("map-link").innerHTML = username + document.getElementById("demo").textContent+"完成："+ latlon;    
     document.getElementById("demo").innerHTML = "";
 }
   
