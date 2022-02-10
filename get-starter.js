91eb5465f03e4b3ef6dba8d5fc00b8cf191f6e21
changmang2003 <changmang.yu@gmail.com>
2022-02-10 14:13:47 +0800

    Update get-starter.js

--- get-starter.js
+++ get-starter.js
@@ -94,10 +94,10 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    var script=document.createElement("script");
-    script.type="text/javascript";
-    script.src="jquery.js";
-    document.getElementsByTagName('head')[0].appendChild(script);
+    //var script=document.createElement("script");
+    //script.type="text/javascript";
+    //script.src="jquery.js";
+    //document.getElementsByTagName('head')[0].appendChild(script);
     var bubble = {};  
     var getUrlString = location.href;
     var url = new URL(getUrlString);  
