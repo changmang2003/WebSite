f3ff0b7c13b22284396a11c79e2d3e95f2370a43
changmang2003 <changmang.yu@gmail.com>
2022-02-10 17:41:25 +0800

    Update get-starter.js

--- get-starter.js
+++ get-starter.js
@@ -112,7 +112,7 @@ function showPosition(position) {
     //let date = new Date(position.timestamp );
 
     const profile = liff.getProfile();
-      toggleProfileData();
+      
       bubble = 
       {        
         events:{
@@ -142,7 +142,7 @@ function showPosition(position) {
   
   $.get("https://script.google.com/macros/s/AKfycbw1X6eY1UFUTQnuxXmqEj82BiiymItZae66x89OoKz-UNE4e-9FH4AyFx9iHgXL3pz6/exec", parameter);
     document.getElementById("map-link").innerHTML = document.getElementById("demo").textContent+"完成："+ latlon ;    
-    document.getElementById("demo").innerHTML = JSON.stringify(profile);
+    document.getElementById("demo").innerHTML = profile;
 }
   
 function showError(error) {
