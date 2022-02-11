457a67bffb1ab1a55987a96713ad050f4893714f
changmang2003 <changmang.yu@gmail.com>
2022-02-11 14:51:23 +0800

    Update get-starter.js

--- get-starter.js
+++ get-starter.js
@@ -21,7 +21,7 @@ function initializeApp(data) {
       .catch((err) => {
         console.log('error', err);
     });
-    window.alert("VAR:2.16 "+  document.getElementById("linename").textContent);
+    window.alert("VAR:2.17 "+  document.getElementById("linename").textContent);
      
 
     // work call
