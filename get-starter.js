505a98fe2c7f9472f45d326e985f606a4431140f
changmang2003 <changmang.yu@gmail.com>
2022-02-11 13:58:39 +0800

    Update get-starter.js

--- get-starter.js
+++ get-starter.js
@@ -17,11 +17,12 @@ function initializeApp(data) {
       .then(profile => {
         const name = profile.displayName;
         document.getElementById("linename").innerHTML = profile.displayName;
+        window.alert("VAR:2.13 "+  profile.displayName);
       })
       .catch((err) => {
         console.log('error', err);
     });
-    window.alert("VAR:2.13 "+  name);
+    
     window.alert("VAR:2.14 "+  document.getElementById("linename").textContent); 
 
     // work call
